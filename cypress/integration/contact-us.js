bcb9bfe05cd17e2d886a9894013cccc17ed57f71
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-06-02 13:38:44 -0300

    34. Creating Our Second Cypress Test - (Negative Test Case)

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -3,6 +3,7 @@
 const doc = "https://docs.cypress.io/api/table-of-contents";
 const urls = [
   "http://www.webdriveruniversity.com/",
+  "http://www.webdriveruniversity.com/Contact-Us/contactus.html",
   "https://www.automationteststore.com/",
 ];
 
@@ -10,16 +11,21 @@ describe("Test Contact Us form via WebdriverUni", () => {
   let webdriveruniversity = "";
 
   beforeEach(() => {
-    webdriveruniversity = urls[0];
+    webdriveruniversity = urls[1];
     cy.visit(webdriveruniversity);
   });
 
   it("Should be able to submit a successful submission via contact us form", () => {
-    // cypress code
-    cy.get("#contact-us > .thumbnail").click();
+    cy.get('[name="first_name"]').type("Cristiano Alexandre");
+    cy.get('[name="last_name"]').type("Faria");
+    cy.get('[name="email"]').type("cristiano@email.com");
+    cy.get("textarea.feedback-input").type("test only");
+    cy.get('[type="submit"]').click();
   });
 
   it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-    // cypress code
+    cy.get('[name="first_name"]').type("Cristiano Alexandre");
+    cy.get('[name="last_name"]').type("Faria");
+    cy.get('[type="submit"]').click();
   });
 });

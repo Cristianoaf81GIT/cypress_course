16f7fb6f2b4624a878668700d3e5edc9428ec8c2
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-07-07 14:00:39 -0300

    46. Improving our Tests with Dynamic Selectors

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -12,8 +12,8 @@ describe("Test Contact Us form via Automation Test Store", () => {
   });
 
   it("Should be able to submit a successfull submission via contact us form", () => {
-    // cy.get(".info_links_footer > :nth-child(5) > a").click();
-    cy.xpath("//a[contains(@href, 'contact')]").click();
+    cy.get("a[href$='contact']").click();
+    // cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get("#ContactUsFrm_first_name").type("Joe");
     cy.get("#ContactUsFrm_email").type("joe_blog@gmail.com");
     cy.get("#ContactUsFrm_enquiry").type(

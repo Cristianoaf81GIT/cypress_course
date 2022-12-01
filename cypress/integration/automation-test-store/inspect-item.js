b0b08b2cc715929176616ec9b62b12d59c32036c
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2022-12-01 07:28:55 -0300

    71. More Examples of Command Chaining - get, find, contains, eq - Part 2/2

--- cypress/integration/automation-test-store/inspect-item.js
+++ cypress/integration/automation-test-store/inspect-item.js
@@ -18,10 +18,12 @@ describe("Inspect Automation Test Store items using chain of commands", () => {
   it("Click on the first item using item header", () => {
       cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();           
   });
-
-   it("Click on the first item usind item text", () => {
-      cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click();
-   });
-
-
+  
+  it("Click on the first item using item text", () => {
+     cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click();
+  });
+  
+  it.only("Click on the first item using item index", () => {
+    cy.get('.fixed_wrapper').find('.prdocutname').eq(0).click();
+  });
 });

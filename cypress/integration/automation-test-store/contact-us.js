b0b6f0dea47628132873ac8187ffe78f5824b6fb
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2023-01-19 07:36:49 -0300

    80. then Command

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -15,7 +15,7 @@ describe("Test Contact Us form via Automation Test Store", () => {
     cy.visit(urls[2]);
   });
 
-  it("Should be able to submit a successfull submission via contact us form", () => {
+  it.only("Should be able to submit a successfull submission via contact us form", () => {
     cy.get("a[href$='contact']").click();
     // cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get("#ContactUsFrm_first_name").type("Joe");
@@ -26,16 +26,19 @@ describe("Test Contact Us form via Automation Test Store", () => {
       "Do you provide additional discount on bulk orders?"
     );
     cy.get(".col-md-6 > .btn").click();
-    cy.get('.mb40 > :nth-child(3)').should('have.text','Your enquiry has been successfully sent to the store owner!'
-);
+    cy.get('.mb40 > :nth-child(3)').should(
+      'have.text',
+      'Your enquiry has been successfully sent to the store owner!'
+    );
+    cy.log("Test has completed!");
   });
 
   it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        // cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
-        cy.get('[name="first_name"]').type("Tom");
-        cy.get('[name="last_name"]').type("blogs");
-        cy.get('textarea.feedback-input').type("How can I learn Cypress?")
-        cy.get('[type="submit"]').click();
-    });
+    // cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.get('[name="first_name"]').type("Tom");
+    cy.get('[name="last_name"]').type("blogs");
+    cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+    cy.get('[type="submit"]').click();
+  });
 
 });
--- cypress/integration/automation-test-store/inspect-item.js
+++ cypress/integration/automation-test-store/inspect-item.js
@@ -16,11 +16,18 @@ describe("Inspect Automation Test Store items using chain of commands", () => {
   });
 
   it("Click on the first item using item header", () => {
-      cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();           
+    cy.get(
+      '#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname'
+    ).click();
   });
   
-  it("Click on the first item using item text", () => {
-     cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click();
+  it.only("Click on the first item using item text", () => {
+    cy
+      .get('.prdocutname')
+      .contains('Skinsheen Bronzer Stick')
+      .click().then(function(itemHeaderText) {
+        console.log(`Select the following item: ${itemHeaderText.text()}`);
+     });    
   });
   
   it.only("Click on the first item using item index", () => {

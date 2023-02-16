544e7beeab9a27b2f327d9067d25cf86f023bcae
Cristiano Alexandre De Faria <cristianoaf81@gmail.com>
2023-02-16 17:17:35 -0300

    88 - iterating through elements

---
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress"/>
+//
+
+describe("Iterate over elements", () => {
+
+  it("Log information of all hair care products", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+
+  });
+  
+  it("Add specific product to basket", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+
+  });
+
+});
+
+

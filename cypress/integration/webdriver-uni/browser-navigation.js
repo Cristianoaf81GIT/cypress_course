22d4b5ae0fda59b7c882f5a38915829ed35bd171
cristiano alexandre de faria <cristianoaf81@gmail.com>
2023-06-20 13:16:56 -0300

    107. Back, Forward, Reload - Part 1/2

---
+++ cypress/integration/webdriver-uni/browser-navigation.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress"/>
+
+const doc = "https://docs.cypress.io/api/table-of-contents";
+const urls = [
+  "http://www.webdriveruniversity.com/",
+  "http://www.webdriveruniversity.com/Contact-Us/contactus.html",
+  "https://www.automationteststore.com/",
+];
+
+describe("Validate webdriver uni homepage links", () => {
+  // let webdriveruniversity = "";
+ 
+  it("Confirm links redirect to correct pages", () => {
+    cy.visit(urls[0]);
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({force: true});
+    cy.url().should('include', 'contactus');
+
+    // perform browser actions
+    cy.go('back');
+    // reload de page
+    cy.reload();
+    // cy.reload(true); // reload page without using cache
+    cy.go('forward'); // return to homepage
+    cy.url().should('include', 'contactus');
+  });
+});

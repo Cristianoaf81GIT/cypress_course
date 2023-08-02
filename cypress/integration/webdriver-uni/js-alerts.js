e3a17591ec9d6e8341deb34716fcf44ec0ddf7e5
cristiano alexandre de faria <cristianoaf81@gmail.com>
2023-08-02 07:34:49 -0300

    114. Alert - Challenge

--- cypress/integration/webdriver-uni/js-alerts.js
+++ cypress/integration/webdriver-uni/js-alerts.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress"/>
 
-
 describe('Handle js Alerts', () => {
   
   it('Confirm js alert contains the correct text', () => {
@@ -12,5 +11,23 @@ describe('Handle js Alerts', () => {
       expect(str).to.equal('I am an alert box!')
     })
   }) 
+  
+  it('Validate js confirm alert box works correctly when clicking ok', () => {
+    cy.visit('http://www.webdriveruniversity.com')
+    cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force: true})
+    cy.get('#button4').click()
+    cy.on('window:confirm', (str) => {
+      expect(str).to.contains('Press a button!')
+    })
+    cy.get('#confirm-alert-text').contains('You pressed OK!')    
+  })
+
+   it('Validate js confirm alert box works correctly when clicking ok', () => {
+    cy.visit('http://www.webdriveruniversity.com')
+    cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force: true})
+    cy.get('#button4').click()
+    cy.on('window:confirm', cy.stub().returns(false)) 
+    cy.get('#confirm-alert-text').contains('You pressed Cancel!')    
+  })
 
 })

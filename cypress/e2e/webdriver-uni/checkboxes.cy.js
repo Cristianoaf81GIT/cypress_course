57a28f3e8815ecdf6afd93ac63c20d566fe79557
cristiano alexandre de faria <cristianoaf81@gmail.com>
2023-09-13 13:08:34 -0300

    119. Handling Checkboxes - Part 1/2

---
+++ cypress/e2e/webdriver-uni/checkboxes.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('Verify checkboxes via webdriveruni', () => {
+  it('check and validate checkbox', () => {
+    cy.visit('http://www.webdriveruniversity.com')
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force: true})
+    cy.get('#checkboxes > :nth-child(1) > input').check()
+    cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
+    cy.get('#checkboxes > :nth-child(3) > input').should('not.be.checked')
+  })
+
+})

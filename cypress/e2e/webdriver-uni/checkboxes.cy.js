ff0c30caa2a0cd3670485ddcf13865ad39722793
cristiano alexandre de faria <cristianoaf81@gmail.com>
2023-09-26 10:36:49 -0300

    123. Selecting Multiple Checkboxes

--- cypress/e2e/webdriver-uni/checkboxes.cy.js
+++ cypress/e2e/webdriver-uni/checkboxes.cy.js
@@ -14,12 +14,20 @@ describe('Verify checkboxes via webdriveruni', () => {
     cy.get('@option-1').check().should('be.checked')
   })
 
-  it.only('uncheck and validate option3 checkbox', () => {
+  it('uncheck and validate option3 checkbox', () => {
     cy.visit('http://www.webdriveruniversity.com')
     cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force: true})
 
     cy.get(':nth-child(5) > input').as('option-3').uncheck()
     cy.get('@option-3').should('not.be.checked')
+  })
+
+  it('Check multiple checkboxes', () => {
+    cy.visit('http://www.webdriveruniversity.com')
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force: true})
+    cy.get('[type="checkbox"]')
+      .check(['option-1','option-2', 'option-3', 'option-4'])
+      .should('be.checked')
   })
 
 })

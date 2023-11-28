25244c80577ce663424485e3d001e47eb0971a87
cristiano alexandre de faria <cristianoaf81@gmail.com>
2023-11-28 06:54:34 -0300

    autocomplete lists

--- cypress/e2e/webdriver-uni/autocomplete-dropdown-list.cy.js
+++ cypress/e2e/webdriver-uni/autocomplete-dropdown-list.cy.js
@@ -7,15 +7,27 @@ describe('Verify Autocomplete dropdown lists via webdriveruni', () => {
     cy.get('#autocomplete-textfield').invoke('removeAttr', 'target').click({force: true})
   
     cy.get('#myInput').type('A')
-    cy.get('#myInputautocomplete-list > *').each(($el, index, $list) => {
+    cy.get('#myInputautocomplete-list > *').each(($el, _index, _$list) => {
       const prod = $el.text()
       const productToSelect = 'Avacado';
       if(prod === productToSelect) {
-        $el.click()
+        // $el.click() /*deprecated*/
+        el.trigger("click")
         cy.get('#submit-button').click()
         cy.url().should('include', productToSelect);        
       }
+    }).then(() => {
+      cy.get('#myInput').type('G')
+      cy.get('#myInputautocomplete-list > *').each(($el, _index, _$list) => {
+          const prod = $el.text()
+          const prodToSelect = 'Grapes'
+          if (prod === prodToSelect) {
+            // $el.click() /*deprecated*/
+            $el.trigger("click")
+            cy.get('#submit-button').click()
+            cy.url().should('include', prodToSelect)
+          }
+      })
     })
-  })
-
+  }) 
 })

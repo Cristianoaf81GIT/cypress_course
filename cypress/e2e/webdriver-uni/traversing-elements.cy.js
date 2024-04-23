3f3f5c89613c9cf3d33211555783d23012c494fa
Cristianoaf81GIT <cristianoaf81@gmail.com>
2024-04-23 07:38:00 -0300

    feat(nextUntil command): add example
    Closes #main

--- cypress/e2e/webdriver-uni/traversing-elements.cy.js
+++ cypress/e2e/webdriver-uni/traversing-elements.cy.js
@@ -44,14 +44,16 @@ describe("Traversing DOM elements in Cypress", () => {
       .should("contain", "Scott");
   });
 
-  it.only("nextAll() to get all of the next sibling DOM elements within elements", () => {
+  it("nextAll() to get all of the next sibling DOM elements within elements", () => {
     cy.get(".traversal-drinks-list")
       .contains("Tea")
       .nextAll()
       .should("have.length", "3");
   });
 
-  it("nextUntil() to get all of the next sibling DOM elements within elements until another element", () => {});
+  it.only("nextUntil() to get all of the next sibling DOM elements within elements until another element", () => {
+    cy.get("#coffee").nextUntil("#milk");
+  });
 
   it("not() to remove DOM element(s) from the set of elements", () => {});
 
